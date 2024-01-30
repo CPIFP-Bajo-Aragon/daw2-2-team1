a344c0048fecf2adbc8926a40334fa505cac25b7
OULEDHAJAR <119263155+OULEDHAJAR@users.noreply.github.com>
2024-01-30 14:37:36 +0100

    cambios iniciador

--- mvc_reto/App/iniciador.php
+++ mvc_reto/App/iniciador.php
@@ -3,10 +3,12 @@
     require_once "config/configurar.php";
     require_once "helpers/funciones.php";
 
-    require_once "helpers/mailer.php";
+    require_once "helpers/Mailer.php";
 
     
     require_once "librerias/Base.php";
     require_once "librerias/Controlador.php";
     require_once "librerias/Core.php";
     require_once "librerias/Sesion.php";
+
+
