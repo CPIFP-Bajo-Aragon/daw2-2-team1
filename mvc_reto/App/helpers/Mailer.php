7b5275cf3ee611d68a69cf5b7590a23dd98c410a
OULEDHAJAR <119263155+OULEDHAJAR@users.noreply.github.com>
2024-01-30 22:49:06 +0100

    Eliminar carpeta mailer y modificaciones en Mailer

--- mvc_reto/App/helpers/Mailer.php
+++ mvc_reto/App/helpers/Mailer.php
@@ -11,37 +11,8 @@ Class Mailer {
   function __construct() {
   }
 
-  // public static function sendEmail($dest, $name) {
-    
-  //   $mail = new PHPMailer();
-  //       $mail->isSMTP();
-  //       $mail->Host = 'in-v3.mailjet.com';
-  //       $mail->SMTPAuth = true;
-  //       $mail->Port = 587;
-  //       $mail->Username = '0506d5e7ba41e6a17fb431ae51df94a9';
-  //       $mail->Password = 'fa7c8689a33a30183b969fd9374637ca';
-  //       $mail->SMTPKeepAlive = true;
 
-  //       $mail->setFrom('houledjillali@gmail.com', 'Comarca del Bajo Aragon');
-  //       $mail->Subject = "Has recibido una nueva notificación en nuestra plataforma";
 
-  //       $userEmail = $dest;
-  //       $userName = $name;
-
-  //       $mail->addAddress($userEmail, $userName);
-
-  //       $mail->Body = "<h2>¡Hola, {$userName}!</h2> <p>Tu cuenta ha sido dada de alta con éxito en nuestra plataforma.</p>";
-  //       $mail->AltBody = "Hello, {$userName}! \n How are you?";
-
-  //       try {
-  //           $mail->send();
-  //           echo "Message sent to: ({$userEmail}) {$mail->ErrorInfo}\n";
-  //       } catch (Exception $e) {
-  //           echo "Mailer Error ({$userEmail}) {$mail->ErrorInfo}\n";
-  //       }
-  //       $mail->clearAddresses();
-  //       $mail->smtpClose();
-  //   }
   public static function sendEmail($dest, $name) {
     $mail = new PHPMailer();
     $mail->isSMTP();
@@ -49,20 +20,20 @@ Class Mailer {
     $mail->Host = 'in-v3.mailjet.com';
     $mail->SMTPAuth = true;
     $mail->Port = 587;
-    $mail->Username = 'e99e6217ffaa65529d8d2fbd7df65fcb';
-    $mail->Password = '9d7e61587469ba30136760628f83db58';
+    $mail->Username = '7cf95e08fc79e0eb7ef9a3446e3b2e79';
+    $mail->Password = '7d279869d526c5ba5cb5c4f7429ffa96';
     $mail->SMTPKeepAlive = true;
-    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
 
-    $mail->setFrom('adriperezcaspe@gmail.com', 'Comarca del Bajo Aragon');
-    $mail->Subject = "Has recibido una nueva notificación en nuestra plataforma.";
+    $mail->setFrom('revitalizona@gmail.com', 'Comarca del Bajo Aragon');
+    $mail->Subject = "Tienes un nuevo aviso de la plataforfa.";
 
     $userEmail = $dest;
     $userName = $name;
 
     $mail->addAddress($userEmail, $userName);
 
-    $mail->Body = "<h2>¡Hola, {$userName}!</h2> <p>Tu cuenta ha sido dada de alta con exito en nuestra plataforma.</p>";
+    $mail->Body = "<h2>¡Hola, {$userName}!</h2> <p>Tu cuenta ha sido creada con exito.</p>";
     $mail->AltBody = "Hello, {$userName}! \n How are you?";
     try {
         $mail->send();
