527145dcfa169d83f94c52d8dc826c947275ba9c
EduardoLizanaAlcon <132059930+EduardoLizanaAlcon@users.noreply.github.com>
2024-02-02 04:38:10 +0100

    Actualizacion Modelos-Vistas-Configuracion

--- mvc_reto/App/modelos/ServiciosModelo.php
+++ mvc_reto/App/modelos/ServiciosModelo.php
@@ -7,12 +7,17 @@
             $this->db = new Base;
         }
 
-        public function listarServicio($id_municipio){
+        public function listarServicioempresas($id_municipio, $id_servicio){
             $this->db->query("SELECT * FROM `DISPONER_SERVICIO` 
                                 INNER JOIN SERVICIO on SERVICIO.id_servicio=DISPONER_SERVICIO.id_servicio 
-                                where DISPONER_SERVICIO.id_municipio= :id;");
+                                where DISPONER_SERVICIO.id_municipio= :id and DISPONER_SERVICIO.id_servicio= :id_servicio;");
             $this->db->bind(':id', $id_municipio);
+            $this->db->bind(':id_servicio', $id_servicio);
         
+            return $this->db->registros();
+        }
+        public function listarServicio(){
+            $this->db->query("SELECT * FROM SERVICIO");      
             return $this->db->registros();
         }
        
--- mvc_reto/App/vistas/servicios/listarservicios.php
+++ mvc_reto/App/vistas/servicios/listarservicios.php
@@ -11,7 +11,7 @@
             <?php }?>
         </select>
     </div>
-    <div class="mb-3 col-11" id="ContenedorServicios">
+    <div class="mb-3 col-11 d-flex flex-wrap" id="ContenedorServicios">
         
     </div>
 </div>
@@ -47,28 +47,79 @@ require_once RUTA_APP . '/vistas/inc/footer.php';
         for (var i = 0; i < serviciosFiltrados.length; i++) {
             
                 var contenedorpueblo = document.createElement('div');
-                contenedorpueblo.classList.add("col-12");
+                contenedorpueblo.classList.add("col-6");
                 contenedorpueblo.setAttribute("id", "pueblo" + i);
+
+                var contenedormapa = document.createElement('div');
+                contenedormapa.setAttribute("id", "mi_mapa");
+                contenedormapa.setAttribute("class", "col-6 w-50 h-50");
 
                 var NombrePueblo = document.createElement('h1');
                 NombrePueblo.textContent = serviciosFiltrados[i].nombre_municipio;
 
                 contenedorpueblo.appendChild(NombrePueblo);
                 contenedorSer.appendChild(contenedorpueblo);
+                contenedorSer.appendChild(contenedormapa);
+                
+                let map = L.map('mi_mapa').setView([serviciosFiltrados[i].lat, serviciosFiltrados[i].log], 16)
 
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
             
                 for (var j = 0; j < serviciosFiltrados[i].servicios.length; j++) {
 
                     var servi = serviciosFiltrados[i].servicios[j];
 
                     var contenedorservicio = document.createElement('div');
-                    contenedorservicio.classList.add("col-4");
                     contenedorservicio.setAttribute("id", "servicio" + i+j);
+                    contenedorservicio.setAttribute("class", "col-5 border m-1");
 
-                    var nombreservicio = document.createElement('h1');
+                    var nombreservicio = document.createElement('h2');
+                    nombreservicio.setAttribute("class", "text-center");
                     nombreservicio.textContent = servi.nombre_servicio;
 
                     contenedorservicio.appendChild(nombreservicio);
+
+                        var contenedorempresas = document.createElement('div');
+                        contenedorempresas.setAttribute("class", "d-flex flex-wrap");
+
+
+                    for (var h = 0; h < serviciosFiltrados[i].servicios[j].empresas.length; h++) {
+
+                        var empresa = serviciosFiltrados[i].servicios[j].empresas[h];
+
+                        var contenedorempresa = document.createElement('div');
+                        contenedorempresa.setAttribute("id", "servicio" + i+j);
+                        contenedorempresa.setAttribute("class", "col-5 border m-4");
+
+                        var nombreempresa = document.createElement('h3');
+                        nombreempresa.setAttribute("class", "text-center");
+                        nombreempresa.textContent = empresa.Nombre_da_servicio;
+
+                        var descripcionempresa = document.createElement('p');
+                        descripcionempresa.textContent = "Descripción: "+empresa.descripcion;
+
+                        var telefonoempresa = document.createElement('p');
+                        telefonoempresa.textContent = "Telefono: "+empresa.telefono;
+
+                        var direccionempresa = document.createElement('p');
+                        direccionempresa.textContent = "Dirección: "+empresa.direccion;
+
+                        contenedorempresa.appendChild(nombreempresa);
+                        contenedorempresa.appendChild(telefonoempresa);
+                        contenedorempresa.appendChild(direccionempresa);
+                        contenedorempresa.appendChild(descripcionempresa);
+
+                        contenedorempresas.appendChild(contenedorempresa);
+                        
+                        L.marker([empresa.latitud, empresa.longitud]).addTo(map).bindPopup("Zócalo de la Ciudad de México");
+
+
+                        map.on('click', onMapClick);
+
+                    }
+                    contenedorservicio.appendChild(contenedorempresas);
 
                     contenedorSer.appendChild(contenedorservicio);
 
@@ -78,6 +129,12 @@ require_once RUTA_APP . '/vistas/inc/footer.php';
             
         }
     }
+
+function onMapClick(e) {
+    alert("Posición: " + e.latlng)
+}
+
+
 </script>
 </body>
 </html>
